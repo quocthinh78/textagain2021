9e50a0a8c18d36b19f4f5b17124c9f0de3d3610c
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-04-17 14:16:28 +0700

    second commit

---
+++ examples/counter-hot/CounterControls.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Value: {{ count }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+    <div>
+      <div>Recent History (last 5 entries): {{ recentHistory }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: {
+      recentHistory(){
+        return this.$store.getters.recentHistory
+      },
+      count(){ return this.$store.getters.count}
+    },
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
